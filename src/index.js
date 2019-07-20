858c94f67d1696750c87868024450b6343d55ac5
Gaurav Sharma <gauravsharma10167@gmail.com>
2019-07-20 18:58:42 +0530

    Second commit

--- src/index.js
+++ src/index.js
@@ -6,15 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT
 
-const multer = require('multer')
-const upload = multer({
-    dest : 'images'
-})
-
-app.post('/upload',upload.single('upload'), async (req, res) => {
-    res.send()
-})
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
